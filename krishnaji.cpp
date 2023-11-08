73eb79ce1c4b72d15a08ed3e3bd58ad0d62a09ae
kris1729suraj <147723116+kris1729suraj@users.noreply.github.com>
2023-11-08 15:24:51 +0530

    ha maine phir update kiya

---
+++ krishnaji.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+#define ll long long
+#define F first
+#define S second
+#define pub push_back
+#define pob pop_back
+ 
+int  main() 
+{
+ios::sync_with_stdio(0);
+cin.tie(0); 
+ 
+
+ 
+}
